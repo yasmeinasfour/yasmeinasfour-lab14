6175c330bf553711a7d5c3c1c0534a107006f644
yasmeinasfour <yasfour04@gwu.edu>
2025-04-13 11:50:31 -0400

    constructor and server

--- Server.java
+++ Server.java
@@ -1,3 +1,47 @@
+import java.io.*;
+import java.net.*;
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.*;
+
 public class Server {
-    
+    private ServerSocket serverSocket;
+    private boolean running;
+    private ArrayList<LocalDateTime> connectedTimes;
+    private ExecutorService threadPool;
+
+
+
+    public Server(int port) {
+        try {
+            this.serverSocket = new ServerSocket(port);
+            this.running = true;
+            this.connectedTimes = new ArrayList<>();
+            this.threadPool = Executors.newCachedThreadPool();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void serve(int numClients) {
+        for (int i = 0; i < numClients && running; i++) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                LocalDateTime connectionTime = LocalDateTime.now();
+                connectedTimes.add(connectionTime);
+
+                Runnable task = new Runnable() {
+                    public void run() {
+                        handleClient(clientSocket);
+                    }
+                };
+                threadPool.execute(task);
+            } catch (IOException e) {
+                if (running) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
 }
